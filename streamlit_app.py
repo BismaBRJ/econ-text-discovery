85a1eccb996cdc7526ee55b2cd6add1c9f0eed65
BismaBRJ <bismabrj@gmail.com>
2026-07-01 19:41:41 +0700

    Previous but misguided attempt for clean invalid-API-key error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
+# note to self:
+# - the entire script is rerun after every interaction!
+# - only data in st.session_state still exists between reruns
+
+import os
+# from llama_index.core.workflow import Context
+from llama_index.core import load_index_from_storage, StorageContext
+from llama_index.llms.google_genai import GoogleGenAI
+from google.genai.errors import APIError, ClientError
 
 st.title("Echo bot")
 
-# set up chat history if not already
+# set up chat history if not already (if first run)
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -16,14 +25,35 @@ canceled = """byok_radio = st.sidebar.radio(
     ("Use from secrets.toml", "Use your own")
 )
 """
-byok_field = st.sidebar.text_input(
+API_KEY = st.sidebar.text_input(
     label="Enter Gemini API key",
     value=""
 )
-model_radio = st.sidebar.radio(
+
+MODEL_NAME = st.sidebar.radio(
     "Choose model",
     ("gemini-3.5-flash", "gemini-3.1-flash-lite")
 )
+
+# well, you can switch LLMs, so yes this is rerun...
+llm_connect_success = False
+response = ""
+if (API_KEY != ""):
+    st.session_state.llm = GoogleGenAI(
+        model=MODEL_NAME,
+        api_key=API_KEY
+    )
+    client = st.session_state.llm._client
+    try:
+        client.models.list(config={"page_size": 1})
+    except APIError as e:
+        response = f"Invalid API key:\n{e}"
+        # llm_connect_success = False
+    except Exception as e:
+        response = f"Unexpected error:\n{e}"
+        # llm_connect_success = False
+    else:
+        llm_connect_success = True
 
 prompt = st.chat_input("Say something :D")
 if (prompt != None): # if the user hits send with nonempty prompt
@@ -39,8 +69,9 @@ if (prompt != None): # if the user hits send with nonempty prompt
     )
 
     # formulate response
-    response = """Why hello there!
-    Let me repeat what you just said:\n""" + prompt
+    if response == "":
+        response = """Why hello there!
+        Let me repeat what you just said:\n""" + prompt
 
     # display response as assistant-sent chat message
     st.chat_message("assistant").markdown(response)
